2c36ce332f97bfad28ebface29b947085b4d6381
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-09-13 09:27:21 +0100

    Moved to HAPI FHIR 4.0.3 and sonar changes

--- src/main/java/uk/gov/wildfyre/nrl/support/CareConnectConstants.java
+++ src/main/java/uk/gov/wildfyre/nrl/support/CareConnectConstants.java
@@ -1,9 +1,17 @@
-package uk.gov.wildfyre.gpcadaptor.support;
+package uk.gov.wildfyre.nrl.support;
 
-public final static class CareConnectConstants {
+public final class CareConnectConstants {
 
-    public final static class IdentifierSystem {
-        public final static nhsNumber =     
+    private CareConnectConstants() { }
+
+    public static final  class IdentifierSystem {
+
+        private IdentifierSystem () {}
+
+        public final static String NHS_NUMBER =  "https://fhir.nhs.uk/Id/nhs-number";
+
+        public final static String SDS_ORGANISATION_CODE = "https://fhir.nhs.uk/Id/ods-organization-code";
     }
 
+    public final static String JSON_CONTENT_TYPE = "application/json+fhir;charset=UTF-8";
 }
