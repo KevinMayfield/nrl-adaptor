319085b4d3d746e17acc9f7ae764c6c701f444a7
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-09-13 09:46:21 +0100

    Moved to HAPI FHIR 4.0.3 and sonar changes

--- src/main/java/uk/gov/wildfyre/nrl/support/NHSDigitalSpineConstants.java
+++ src/main/java/uk/gov/wildfyre/nrl/support/NHSDigitalSpineConstants.java
@@ -7,6 +7,9 @@ public final class NHSDigitalSpineConstants {
     }
 
     public static final  class Url {
+
+        private Url () {}
+
             public static final String BASE = "https://demographics.spineservices.nhs.uk/STU3/";
 
             public static final String PATIENT = "https://demographics.spineservices.nhs.uk/STU3/Patient/";
