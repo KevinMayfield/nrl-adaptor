8fcf8f16b00d62352648b3d56a2d3d7c86ec2509
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2019-09-16 15:52:19 +0100

    Moved to HAPI FHIR 4.0.3 and sonar changes

--- src/main/java/uk/gov/wildfyre/nrl/CustomRestfulServer.java
+++ src/main/java/uk/gov/wildfyre/nrl/CustomRestfulServer.java
@@ -7,6 +7,7 @@ import ca.uhn.fhir.rest.server.FifoMemoryPagingProvider;
 import ca.uhn.fhir.rest.server.HardcodedServerAddressStrategy;
 import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.interceptor.CorsInterceptor;
+import ca.uhn.fhir.rest.server.interceptor.LoggingInterceptor;
 import ca.uhn.fhir.util.VersionUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.cors.CorsConfiguration;
@@ -88,6 +89,9 @@ public class CustomRestfulServer extends ca.uhn.fhir.rest.server.RestfulServer {
         // Create the interceptor and register it
         CorsInterceptor interceptor = new CorsInterceptor(config);
         getInterceptorService().registerInterceptor(interceptor);
+
+        LoggingInterceptor loggingInterceptor = new LoggingInterceptor();
+        getInterceptorService().registerInterceptor(loggingInterceptor);
 
         FifoMemoryPagingProvider pp = new FifoMemoryPagingProvider(10);
         pp.setDefaultPageSize(10);
